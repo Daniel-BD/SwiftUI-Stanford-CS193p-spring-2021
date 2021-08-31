6de0539b4d0bcc5d76d156a447780e82ec8c1abc
Daniel Duvanå <daniel.b.olofsson@gmail.com>
2021-08-31 16:18:03 +0200

    Added MVVM

---
+++ Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,31 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Daniel Duvanå on 2021-08-31.
+//
+
+import SwiftUI
+
+class EmojiMemoryGame: ObservableObject {
+    static let emojis = ["🚕","🚌","🚓","🚑","🚒","🚜","🚚","🚛","🚠","🚋","🚄","✈️","🛳","🚁","🚂"]
+    
+    static func createMemoryGame() -> MemoryGame<String> {
+        MemoryGame<String>(numberOfPairsOfCards: 4) { pairIndex in
+            emojis[pairIndex]
+        }
+    }
+    
+    @Published private var model: MemoryGame<String> = createMemoryGame()
+    
+    var cards: Array<MemoryGame<String>.Card> {
+        return model.cards
+    }
+    
+    // MARK: - Intent(s)
+    
+    func choose(_ card: MemoryGame<String>.Card) {
+
+        model.choose(card)
+    }
+}
